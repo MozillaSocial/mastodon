a9b14cc04216cc1d9dfba92fbf4cd4c843cd6407
Amri Toufali <amri@handsomemedia.com>
2023-08-11 15:18:55 -0700

    Add direct link to the Single-Sign On provider if there is only one sign up method available (#26083)

--- app/javascript/mastodon/initial_state.js
+++ app/javascript/mastodon/initial_state.js
@@ -81,6 +81,7 @@
  * @property {boolean} use_blurhash
  * @property {boolean=} use_pending_items
  * @property {string} version
+ * @property {string} sso_redirect
  */
 
 /**
@@ -136,5 +137,6 @@ export const version = getMeta('version');
 export const languages = initialState?.languages;
 // @ts-expect-error
 export const statusPageUrl = getMeta('status_page_url');
+export const sso_redirect = getMeta('sso_redirect');
 
 export default initialState;
