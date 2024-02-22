1ca819081936fb795466ef70dcb8cb63242143ff
Amri Toufali <amri@handsomemedia.com>
2024-02-22 11:14:07 -0800

    differentiate envs for better Sentry logging

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
 
+# RAILS_ENV is set to 'production' for both stage and prod servers.
+# We use LOCAL_DOMAIN to differentiate for better Sentry logging.
+local_domain = ENV.fetch('LOCAL_DOMAIN', '')
+
 Sentry.init do |config|
+  config.environment = if local_domain['mozilla.social']
+                         'production'
+                       elsif local_domain['stage']
+                         'staging'
+                       else
+                         'development'
+                       end
+
   config.dsn = 'https://def412573e2b44d39e73a130b09bb25a@o1069899.ingest.sentry.io/4506591637471232'
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
