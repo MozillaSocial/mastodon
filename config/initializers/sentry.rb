3c53b309ec3e40fb5e7cd912d8baf8bc89075a75
Amri Toufali <amri@handsomemedia.com>
2024-02-22 12:11:55 -0800

    update Sentry DSN

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-Sentry.init do |config|
-  config.dsn = 'https://def412573e2b44d39e73a130b09bb25a@o1069899.ingest.sentry.io/4506591637471232'
+sentry_dsn = ENV.fetch('SENTRY_DSN', nil)
+
+sentry_dsn && Sentry.init do |config|
+  config.dsn = sentry_dsn
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
   # Set traces_sample_rate to 1.0 to capture 100%
